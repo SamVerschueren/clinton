7d75dc1399f0b407dee04deabbe235091a4f1570
Sam Verschueren <sam.verschueren@gmail.com>
2015-12-20 19:26:28 +0100

    :arrow_up: string-occurrence@1.1.1

--- test.js
+++ test.js
@@ -4,3 +4,7 @@ import fn from './';
 test('throw if language is not javascript', async t => {
 	await t.throws(fn('SamVerschueren/BB10-OAuth'), 'We can only validate JavaScript projects.');
 });
+
+test('caching', async () => {
+	console.log(await fn('sindresorhus/fs-access'));
+});
