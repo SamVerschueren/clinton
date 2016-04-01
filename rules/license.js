f08de8d5ce2288350315c6b69636e004a9224f59
Sam Verschueren <sam.verschueren@gmail.com>
2016-04-01 21:36:47 +0200

    rephrase license rule error

--- rules/license.js
+++ rules/license.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = function (ctx) {
 	if (!ctx.files.some(file => file.toLowerCase().indexOf('license') === 0)) {
-		throw new Error('Make sure to add a `license` to your project');
+		throw new Error('Missing `license` file');
 	}
 };
