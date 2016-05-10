6b2b4f1926eadcfa619264404508c1120a60bbd5
Sam Verschueren <sam.verschueren@gmail.com>
2016-05-10 19:01:22 +0200

    add extra tests

--- test/test.js
+++ test/test.js
@@ -5,6 +5,14 @@ test('project does not exist', async t => {
 	t.throws(m('foo/bar'), 'Path foo/bar does not exist.');
 });
 
+test('no project provided', async t => {
+	t.throws(m(), 'No input provided.');
+});
+
 test('no errors', async t => {
 	t.is((await m('../')).length, 0);
+});
+
+test('cwd option', async t => {
+	t.is((await m('.', {cwd: '../'})).length, 0);
 });
