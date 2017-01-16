4b87c66f76b8f2256827a5747db817a38e2d4dee
Sam Verschueren <sam.verschueren@gmail.com>
2017-01-16 17:58:59 +0100

    cache the package schema for one day - fixes #61

--- rules/pkg-schema.js
+++ rules/pkg-schema.js
@@ -1,5 +1,27 @@
 'use strict';
 const ZSchema = require('z-schema');
+const CacheConf = require('cache-conf');
+
+const cache = new CacheConf();
+
+const SCHEMA_URL = 'http://json.schemastore.org/package';
+const ONE_DAY = 86400000;
+
+const getSchema = ctx => {
+	const cachedSchema = cache.store['pkg-schema'] && cache.store['pkg-schema'].data;
+
+	if (cachedSchema && !cache.isExpired('pkg-schema')) {
+		return Promise.resolve(cachedSchema);
+	}
+
+	return ctx.got.get(SCHEMA_URL)
+		.then(res => {
+			const schema = JSON.parse(res.body);
+			cache.set('pkg-schema', schema, {maxAge: ONE_DAY});
+			return schema;
+		})
+		.catch(() => cachedSchema);
+};
 
 module.exports = ctx => {
 	const validator = new ZSchema();
@@ -7,10 +29,10 @@ module.exports = ctx => {
 
 	return Promise.all([
 		ctx.fs.readFile('package.json'),
-		ctx.got.get('http://json.schemastore.org/package')
+		getSchema(ctx)
 	]).then(res => {
 		const pkg = res[0];
-		const schema = JSON.parse(res[1].body);
+		const schema = res[1];
 
 		const isValid = validator.validate(pkg, schema);
 
